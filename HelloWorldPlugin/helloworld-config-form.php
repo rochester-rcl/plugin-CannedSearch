ce2264575b2df940eeb237229e54a889ccc9a189
Kushal Gautam <kushal.gtm385@gmail.com>
2021-06-03 16:22:23 -0400

    new config file

--- HelloWorldPlugin/helloworld-config-form.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require dirname(__FILE__) . '/forms/MainForm.php';
-?>
-<div id="primary">
-    <?php echo new MainForm(); ?>      <!-- The name convention maybe?? -->
-</div>
---
+++ HelloWorldPlugin/views/admin/plugin/helloworld-config-form.php
@@ -0,0 +1,8 @@
+<h2>Hello World Form</h2>
+<br />
+<div class="field">
+    <div class="inputs one columns omega">
+        <label for="threejs_browse_description"><?php echo __('Browse Template Description'); ?></label>
+        <?php echo $this->formTextarea('threejs_browse_description', get_option('threejs_browse_description')); ?>
+    </div>
+</div>
